8a531b91742071e4389887c0aee6b21a1c39a0fd
melissayork <melissayork_8@hotmail.com>
2024-07-12 11:49:50 -0600

    :+1:

---
+++ paqueteoptimizacion_meliyork/funcionesPrueba/funcionesPrueba/metodosMultivariables/__init__.py
@@ -0,0 +1,6 @@
+from .caminataAleatoria import caminata_aleatoria
+from .cauchy import cauchy
+from .fletcherReeves import fletcherReeves
+from .hookeJeeves import hooke_jeeves
+from .nelderMeadSimplex import nelderMeadSimplex
+from.newton import newton
