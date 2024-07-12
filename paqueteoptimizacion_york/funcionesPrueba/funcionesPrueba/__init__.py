6c0f61941f1cc4bc68e1c79df52a4856b0cdd87e
melissayork <melissayork_8@hotmail.com>
2024-07-12 13:26:05 -0600

    si

--- paqueteoptimizacion_york/funcionesPrueba/funcionesPrueba/__init__.py
+++ paqueteoptimizacion_york/funcionesPrueba/funcionesPrueba/__init__.py
@@ -11,3 +11,6 @@ from .metodosUnivariables.busquedaDorada import busquedaDorada
 from .metodosUnivariables.intervalHalving import interval_halving_method
 from .metodosUnivariables.secante import secante
 from .metodosUnivariables.newtonRaphson import newton_raphson
+
+from .funcionesPrueba.funcionesMultivariables import rastrigin, ackley, himmelblau, sphere, rosenbrock,  beale, goldstein, booth, bunkin, matyas, levi, threehumpcamel,easom,crossintray, eggholder, holdertable, mccormick, schaffer2, schaffer_n4,  styblinskitang, shekel  
+from .funcionesPrueba.funcionesUnivariables import f1, f2, f3, f4
